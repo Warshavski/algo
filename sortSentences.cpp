b81b95c02e04a7e3e5fb43fcd667c904379a4b9c
Warshavski <shikari005@gmail.com>
2015-09-24 13:23:53 +0400

    rewrite text sorting

--- sortSentences.cpp
+++ sortSentences.cpp
@@ -11,18 +11,7 @@ using namespace std;
  *      2. Check memory use
  */
 
-const int BUFF_SIZE = 255;
-
-void printAllText(char **text, int size)
-{
-	for (int i = 0; i < size; ++i)
-	{
-		cout << i << " "  << text[i];	
-		cout << endl;		
-	}
-	cout << endl;
-}
-
+/*
 void printAllText(char **text, int size, char path[])
 {
 	ofstream outfile;
@@ -33,8 +22,9 @@ void printAllText(char **text, int size, char path[])
 		
 	outfile.close();
 }
+*/
 
-// yeap not good (better use list)
+/* yeap, pretty shitty (better use list)
 char **compressArray(char **array, int compressedSize, int currentSize)
 {
 	char **compressedArray = new char*[compressedSize];
@@ -50,8 +40,9 @@ char **compressArray(char **array, int compressedSize, int currentSize)
 		
 	return compressedArray;
 }
+*/
 
-char **readFromFile(char path[], int &sentencesCount)
+/*char **readFromFile(char path[], int &sentencesCount)
 {
 	ifstream textFile;
 	textFile.open(path);
@@ -73,12 +64,12 @@ char **readFromFile(char path[], int &sentencesCount)
 	textFile.close();
 
 	return copressedSentences;
-}
+}*/
 
-
-/* select first word from sentence
- * TODO : add array of the separators
- */
+/*
+/// * select first word from sentence
+// * TODO : add array of the separators
+// * /
 char *selectFirstWord(char *sentence)
 {
 	char *word = new char[BUFF_SIZE];
@@ -91,14 +82,14 @@ char *selectFirstWord(char *sentence)
 	}
 	
 	return word;
-	/*
+	///*
 	char *buff = new char[BUFF_SIZE];
 	strcpy(buff, row);
 	
 	char *pch = strtok(buff, " ");
 	delete [] buff;
 	return pch;
-	*/
+	//* /
 }
 
 void swapElements(char *element1, char *element2)
@@ -110,8 +101,10 @@ void swapElements(char *element1, char *element2)
 	
 	delete [] buff;
 }
+*/
 
-// not goods sorting (too slow)
+/*
+// not so good sorting (too slow)
 void sortSentences(char **arr, int size)
 {
 	for (int i = 0; i < size - 1; ++i)
@@ -124,23 +117,239 @@ void sortSentences(char **arr, int size)
 			swapElements(arr[i], arr[min]);
 	}
 }
+*/
+/*
+void printAllText(char **text, int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << i << " "  << text[i];	
+		cout << endl;		
+	}
+	cout << endl;
+}
+*/
+
+const int BUFF_SIZE = 255;
+const char END_OF_TEXT = '\0';
+
+struct sentence
+{
+	int startPosition;
+	int lenght;
+	
+	/* use constructors or not ?
+	sentence()
+	{
+		startPosition = 0;
+		lenght = 0;
+	}
+	
+	sentence (int sPos, int len)
+	{
+		startPosition = sPos;
+		lenght = len;
+	}
+	*/
+};
+
+void printAllText(char *text)
+{
+	for (int i = 0; text[i] != END_OF_TEXT; ++i)
+		cout << text[i];
+	cout << endl;
+}
+
+
+
+void printAllText(char *text, sentence order[], int size)
+{
+	for (int i = 0; i < size; ++i)
+	{
+		cout << i << " : ";
+		for (int j = order[i].startPosition, k = 0; k < order[i].lenght; ++j, ++k)
+			cout << text[j];
+		cout << endl;
+	}
+}
+
+void printAllText(char *text, char path[])
+{
+	ofstream outfile;
+	outfile.open(path);
+	
+	for (int i = 0; text[i] != END_OF_TEXT; ++i)
+		outfile << text[i];
+		
+	outfile.close();
+}
+
+void printAllText(char *text, sentence order[], int size, char path[])
+{
+	ofstream outfile;
+	outfile.open(path);
+	
+	for (int i = 0; i < size; ++i)
+	{
+		for (int j = order[i].startPosition, k = 0; k < order[i].lenght; ++j, ++k)
+			outfile << text[j];
+		outfile << '\n';
+	}
+		
+	outfile.close();
+}
+
+char *readFromFile(char path[])
+{
+	char *text;
+	long textLength;
+   
+    ifstream textFile;
+	textFile.open(path);
+	
+	bool bRead = false;
+	
+    if(!textFile)
+        cout<< "Error! Can't open <<" << path << "\n";
+    else
+    {
+		/* Sets the position of the next character to be extracted from the input stream.
+		 * position : end of file */
+        textFile.seekg(0, ios::end);
+		
+		/* Returns the position of the current character in the output stream.
+		 * sets textLength */
+        textLength = textFile.tellg();
+		
+		// position : begin of file
+        textFile.seekg(0, ios::beg);
+        
+		/* try to create array for our text
+		 * just allocate memory	*/
+		if(!(text = new char[textLength + 1]))
+            cout<<"Error! Can't allocate memory \n";
+        else
+        {
+            textFile.read(text, textLength);
+			/* sets the null termination character
+			 * It marks the end of the string */
+            text[textLength] = '\0';
+        }
+		
+        bRead = !textFile.bad();
+        textFile.close();
+	}
+	return text;
+}
+
+bool endOfSentence(char currentChar)
+{
+	const int SIZE = 3;
+	char dividers[SIZE] = {'.', '!', '?'};
+	
+	for (int i = 0; i < SIZE; ++i)
+		if (currentChar == dividers[i])
+			return true;
+	
+	return false;
+}
+
+sentence *compressArray(sentence array[], int currentSize, int compressedSize)
+{
+	sentence *compressedArray = new sentence[compressedSize];
+	
+	for (int i = 0 ; i < compressedSize; ++i)
+		compressedArray[i] = array[i];
+	
+	return compressedArray;
+}
+
+/* TODO : 
+ *		1. optimize memory allocation.
+ * 		2. optimize code (check end of text and ignore of ' ' and '\n')
+ *      3. manipulate with pointers instead of objects
+ */ 
+sentence *createSentencesArray(char *text, int &out_sentencesCount)
+{
+	sentence *tempArray = new sentence[BUFF_SIZE];
+	
+	out_sentencesCount = 0;
+	int position = 0;
+	for (int i = 0; text[i] != END_OF_TEXT; ++i)
+	{
+		if (endOfSentence(text[i]))
+		{
+			++i;
+			tempArray[out_sentencesCount].startPosition = position;
+			tempArray[out_sentencesCount].lenght = i - position;
+			++out_sentencesCount;	
+	
+			while (text[i] != END_OF_TEXT && (text[i] == '\n' || text[i] == ' '))
+				++i;
+			position = i;
+		}
+	}
+	
+	sentence *array = compressArray(tempArray, BUFF_SIZE, out_sentencesCount);
+	
+	delete [] tempArray;
+	
+	return array;
+}
+
+
+// on swap better use pointers
+void sortSentences(char *text, sentence order[], int size)
+{
+	for (int i = 0; i < size - 1; ++i)
+	{
+		int minIndex = i;
+		
+		for (int j = i + 1; j < size; ++j)
+			if ((text[order[j].startPosition] < text[order[minIndex].startPosition]))
+				minIndex = j;
+		
+		if (minIndex != i)
+		{
+			sentence buff = order[i];
+			order[i] = order[minIndex];
+			order[minIndex] = buff;
+		}
+			
+	}
+}
+
+//#TEST
+void printSentenceArray(sentence arr[], int size)
+{	
+	for (int i = 0; i < size; ++i)
+		cout << arr[i].startPosition << " | " << arr[i].lenght << endl;
+	cout << endl;
+}
 
 int main()
 {
 	int sentencesCount = 0;
-	char **text = readFromFile("test.txt", sentencesCount);
+	
+	char *text = readFromFile("test2.txt");
 	
 	cout << "Unsorted text : " << endl;
-	printAllText(text, sentencesCount);
+	printAllText(text);
 	
-	sortSentences(text, sentencesCount);
+	cout << '\n' << endl;
+	sentence *arr = createSentencesArray(text, sentencesCount);
 	
-	cout << "Sorted text : " << endl;
-	printAllText(text, sentencesCount);
-	printAllText(text, sentencesCount, "outputText.txt");
+	cout << "unsorted text : " << endl;
+	printAllText(text, arr, sentencesCount);
 	
-	for (int i = 0; i < sentencesCount; ++i)
-		delete [] text[i];
+	sortSentences(text, arr, sentencesCount);
+	
+	cout << endl;
+	cout << "sorted text : " << endl;
+	printAllText(text, arr, sentencesCount);
+	printAllText(text, arr, sentencesCount, "outFile.txt");
+	
+	delete [] text;
 	
 	return 0;
 }
